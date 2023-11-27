85b64466bde02e82bb166bc846d77f54920965d8
Aph3li0s <tramphan2294@gmail.com>
2023-11-27 21:30:56 +0700

    Added back files

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,74 +1,4 @@
 import "./App.css";
-<<<<<<< Updated upstream
-
-function App() {
-  return (
-    <main>
-      <header>
-        <a href="" className="logo">
-          Blog App
-        </a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-      <div className="post">
-        <div className="image">
-          <img src="https://sa.kapamilya.com/absnews/abscbnnews/media/2023/life/11/23/20231123-esports.jpg" />
-        </div>
-        <div className="text">
-          <h2>
-            T1 nắc Weibo Gaming cực căng 3 - 0 tại CKTG Liên Minh Huyền Thoại
-          </h2>
-          <p className="info">
-            <a className="author">Kory the Korgy</a>
-            <time>2023-01-06 16:45</time>
-          </p>
-          <p className="summary">
-            T1 đã giành chiến thắng 3-0 trước WBG và trở thành nhà vô địch CKTG
-            2023. Đây cũng là chức vô địch CKTG thứ 4 của T1 và cá nhân Faker.
-          </p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img src="https://sa.kapamilya.com/absnews/abscbnnews/media/2023/life/11/23/20231123-esports.jpg" />
-        </div>
-        <div className="text">
-          <h2>
-            T1 nắc Weibo Gaming cực căng 3 - 0 tại CKTG Liên Minh Huyền Thoại
-          </h2>
-          <p className="info">
-            <a className="author">Kory the Korgy</a>
-            <time>2023-01-06 16:45</time>
-          </p>
-          <p className="summary">
-            T1 đã giành chiến thắng 3-0 trước WBG và trở thành nhà vô địch CKTG
-            2023. Đây cũng là chức vô địch CKTG thứ 4 của T1 và cá nhân Faker.
-          </p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img src="https://sa.kapamilya.com/absnews/abscbnnews/media/2023/life/11/23/20231123-esports.jpg" />
-        </div>
-        <div className="text">
-          <h2>
-            T1 nắc Weibo Gaming cực căng 3 - 0 tại CKTG Liên Minh Huyền Thoại
-          </h2>
-          <p className="info">
-            <a className="author">Kory the Korgy</a>
-            <time>2023-01-06 16:45</time>
-          </p>
-          <p className="summary">
-            T1 đã giành chiến thắng 3-0 trước WBG và trở thành nhà vô địch CKTG
-            2023. Đây cũng là chức vô địch CKTG thứ 4 của T1 và cá nhân Faker.
-          </p>
-        </div>
-      </div>
-    </main>
-=======
 import { Route, Routes } from "react-router-dom";
 
 import Header from "./components/Home/Header";
@@ -84,7 +14,6 @@ function App() {
         <Route path={"/register"} element={<div>Register</div>} />
       </Route>
     </Routes>
->>>>>>> Stashed changes
   );
 }
 
