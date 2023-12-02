3b9efc8b677e8dde112dae34d0799f57e801fb8d
phamtruc-work <phamtruc.work@gmail.com>
2023-12-02 22:24:41 +0700

    Added Login API
    Added Login API:
    + Check if username and password is correct
    + Check if username and password is empty

--- api/index.js
+++ api/index.js
@@ -2,13 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const { default: mongoose } = require("mongoose");
 const bcrypt = require("bcryptjs");
-
+const jwt = require("jsonwebtoken");
 const User = require("./models/User");
 const app = express();
 
-const salt = bcrypt.genSaltSync(10);
+const passwordSalt = bcrypt.genSaltSync(10);
+const tokenSalt = "bcrypt.genSaltSync(10)";
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 
 mongoose.connect(
@@ -20,13 +21,32 @@ app.post("/register", async (req, res) => {
   try {
     const UserDoc = await User.create({
       username,
-      password: bcrypt.hashSync(password, salt),
+      password: bcrypt.hashSync(password, passwordSalt),
     });
     res.json({ UserDoc });
   } catch (e) {
     res.status(400).json(e);
   }
-  // res.json("test ok");
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(401).json("Username or Password is null");
+  }
+  const UserDoc = await User.findOne({ username });
+
+  const passwordCheck = bcrypt.compareSync(password, UserDoc.password);
+
+  if (passwordCheck) {
+    jwt.sign({ username, id: UserDoc._id }, tokenSalt, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie("token", token).json("ok");
+      res.status(200);
+    });
+  } else {
+    res.status(400).json("Wrong Credentials");
+  }
 });
 
 app.listen(4000);
--- blog-app/src/components/Home/Pages/LoginPage.js
+++ blog-app/src/components/Home/Pages/LoginPage.js
@@ -1,11 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
 
 function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  const usernameSetHandler = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const passwordSetHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
+  async function submitLogin(event) {
+    event.preventDefault();
+    const response = await fetch("http://localhost:4000/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-type": "application/json" },
+      credentials: "include",
+    });
+    switch (response.status) {
+      case 200:
+        setRedirect(true);
+        alert("Login Successful");
+        break;
+      case 400:
+        alert("Login Failed");
+        break;
+      case 401:
+        alert("Username and Password can not be empty");
+        break;
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to={"/"} />;
+  }
   return (
-    <form className="formLogin" action="">
+    <form className="formLogin" onSubmit={submitLogin}>
       <h1>Login</h1>
-      <input type="text" placeholder="username" />
-      <input type="password" placeholder="password" />
+      <input type="text" placeholder="username" onChange={usernameSetHandler} />
+      <input
+        type="password"
+        placeholder="password"
+        onChange={passwordSetHandler}
+      />
       <button>Login</button>
     </form>
   );
