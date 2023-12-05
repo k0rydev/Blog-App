5edd8a6fa5681fb063e2ce16db3eff1dbc291db2
phamtruc-work <phamtruc-work@gmail.com>
2023-12-05 10:29:31 +0700

    Add Edit Post Page

--- api/index.js
+++ api/index.js
@@ -95,6 +95,36 @@ app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
   }
 });
 
+app.put("/post", uploadMiddleware.single("file"), async (req, res) => {
+  const newPath = null;
+  if (req.file) {
+    const { originalname, path } = req.file;
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+    newPath = path + "." + ext;
+    fs.renameSync(path, newPath);
+  }
+  const { token } = req.cookies;
+
+  jwt.verify(token, tokenSecret, {}, async (err, info) => {
+    if (err) throw err;
+    const { id, title, summary, content } = req.body;
+
+    const postDoc = await Post.findById(id);
+    const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+    if (!isAuthor) {
+      res.status(400).json("You are not the author");
+    }
+    await postDoc.updateOne({
+      title,
+      summary,
+      content,
+      cover: newPath ? newPath : postDoc.cover,
+    });
+    res.status(200).json(postDoc);
+  });
+});
+
 app.get("/post", async (req, res) => {
   res.json(
     await Post.find()
@@ -109,4 +139,5 @@ app.get("/post/:id", async (req, res) => {
   const postDoc = await Post.findById(id).populate("author", ["username"]);
   res.json(postDoc);
 });
+
 app.listen(4000);
