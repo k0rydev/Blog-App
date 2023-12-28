c87b275c155b0223d87fa8ebd72a834106bd73ed
Trúc Phạm <phamtruc.main@gmail.com>
2023-12-28 15:34:51 +0700

    Remove redundancy for useState in useUser

--- blog-app/src/adapter/useUser.js
+++ blog-app/src/adapter/useUser.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from "react";
+import { useEffect, useContext } from "react";
 import { UserContext } from "../context/UserContext";
 
 export function useUser() {
